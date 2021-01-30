5731e03c3e29042e8ee9d10a16788bada04120e1
heavy_stone <>
2021-01-30 15:49:05 +0900

    [add]annotate gem

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id         :bigint           not null, primary key
+#  content    :text             not null
+#  end_at     :datetime         not null
+#  name       :string           not null
+#  place      :string           not null
+#  start_at   :datetime         not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  owner_id   :bigint
+#
+# Indexes
+#
+#  index_events_on_owner_id  (owner_id)
+#
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
